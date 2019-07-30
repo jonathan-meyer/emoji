6a23ad1011571d962e10be0f8ada6e73cc5ed496
Jonathan Meyer <jon@stej.com>
2019-07-30 00:29:03 -0400

    more tests

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,46 @@
 import React from "react";
 import { toImage } from "emoji-toolkit";
+import cheerio from "cheerio";
 
 const childrenToImage = children => {
+  let components = [];
+
   if (Array.isArray(children)) {
-    return children.map(child => childrenToImage(child)).join("\n");
+    components.push(children.map(child => childrenToImage(child)));
+  } else if (typeof children === "object") {
+    components.push(childrenToImage(children.props.children));
+  } else if (typeof children === "string") {
+    const html = toImage(children);
+    const $ = cheerio.load(html);
+
+    $("body")
+      .contents()
+      .each(function(key) {
+        if ($(this).hasClass("joypixels")) {
+          const img = $(this);
+
+          components.push(
+            <img
+              key={key}
+              className="joypixels"
+              alt={img.attr("alt")}
+              title={img.attr("title")}
+              src={img.attr("src")}
+            />
+          );
+        } else {
+          components.push(<span key={key}>{$(this).text()}</span>);
+        }
+      });
   }
 
-  if (typeof children === "object") {
-    return toImage(children.props.children);
-  }
-
-  if (typeof children === "string") {
-    return toImage(children);
-  }
+  return components;
 };
 
 class Emoji extends React.Component {
   render() {
     const { children } = this.props;
-
-    return (
-      <span dangerouslySetInnerHTML={{ __html: childrenToImage(children) }} />
-    );
+    return <>{childrenToImage(children)}</>;
   }
 }
 
--- test/test.jsx
+++ test/test.jsx
@@ -1,26 +1,130 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
+import { shallow, mount, render } from "enzyme";
 
 import Emoji from "../src/index";
+import { COPYFILE_EXCL } from "constants";
 
 describe("Emoji", () => {
   it("renders with no children", () => {
-    const component = shallow(<Emoji />);
-    expect(component.exists()).toEqual(true);
+    const component = render(<Emoji />);
+
+    expect(component.text()).toEqual("");
   });
 
   it(`renders with "string" child`, () => {
-    const component = shallow(<Emoji>Hello World</Emoji>);
-    expect(component.exists()).toEqual(true);
+    const component = render(<Emoji>Hello World</Emoji>);
+
+    expect(component.text()).toEqual("Hello World");
   });
 
   it(`renders with "short code" child`, () => {
-    const component = shallow(<Emoji>:smile:</Emoji>);
-    expect(component.exists()).toEqual(true);
+    const component = render(<Emoji>:smile:</Emoji>);
+
+    expect(component.is("img")).toEqual(true);
+    expect(component.attr()).toEqual(
+      expect.objectContaining({
+        class: "joypixels",
+        src:
+          "https://cdn.jsdelivr.net/joypixels/assets/5.0/png/unicode/32/1f604.png",
+        title: ":smile:",
+        alt: "😄"
+      })
+    );
   });
 
   it(`renders with "unicode" child`, () => {
-    const component = shallow(<Emoji>😁️</Emoji>);
-    expect(component.exists()).toEqual(true);
+    const component = render(<Emoji>😄</Emoji>);
+
+    expect(component.is("img")).toEqual(true);
+    expect(component.attr()).toEqual(
+      expect.objectContaining({
+        class: "joypixels",
+        alt: "😄",
+        title: ":smile:",
+        src:
+          "https://cdn.jsdelivr.net/joypixels/assets/5.0/png/unicode/32/1f604.png"
+      })
+    );
+  });
+
+  it(`renders with "text and short code" child`, () => {
+    const component = render(<Emoji>Hello World :smile:</Emoji>);
+
+    expect(component.eq(0).text()).toEqual("Hello World ");
+    expect(component.eq(1).attr()).toEqual(
+      expect.objectContaining({
+        class: "joypixels",
+        src:
+          "https://cdn.jsdelivr.net/joypixels/assets/5.0/png/unicode/32/1f604.png",
+        title: ":smile:",
+        alt: "😄"
+      })
+    );
+  });
+
+  it(`renders with "text and unicode" child`, () => {
+    const component = render(<Emoji>Hello World 😄</Emoji>);
+
+    expect(component.eq(0).text()).toEqual("Hello World ");
+    expect(component.eq(1).attr()).toEqual(
+      expect.objectContaining({
+        class: "joypixels",
+        src:
+          "https://cdn.jsdelivr.net/joypixels/assets/5.0/png/unicode/32/1f604.png",
+        title: ":smile:",
+        alt: "😄"
+      })
+    );
+  });
+
+  it(`renders with "short code and text" child`, () => {
+    const component = render(<Emoji>:smile: Hello World</Emoji>);
+
+    expect(component.eq(1).text()).toEqual(" Hello World");
+    expect(component.eq(0).attr()).toEqual(
+      expect.objectContaining({
+        class: "joypixels",
+        src:
+          "https://cdn.jsdelivr.net/joypixels/assets/5.0/png/unicode/32/1f604.png",
+        title: ":smile:",
+        alt: "😄"
+      })
+    );
+  });
+
+  it(`renders with "unicode and text" child`, () => {
+    const component = render(<Emoji>😄 Hello World</Emoji>);
+
+    expect(component.eq(1).text()).toEqual(" Hello World");
+    expect(component.eq(0).attr()).toEqual(
+      expect.objectContaining({
+        class: "joypixels",
+        src:
+          "https://cdn.jsdelivr.net/joypixels/assets/5.0/png/unicode/32/1f604.png",
+        title: ":smile:",
+        alt: "😄"
+      })
+    );
+  });
+
+  it(`renders with "short code and <i>text</i>" child`, () => {
+    const component = render(
+      <Emoji>
+        :smile: <i>Hello World</i>
+      </Emoji>
+    );
+
+    expect(component.eq(0).attr()).toEqual(
+      expect.objectContaining({
+        class: "joypixels",
+        src:
+          "https://cdn.jsdelivr.net/joypixels/assets/5.0/png/unicode/32/1f604.png",
+        title: ":smile:",
+        alt: "😄"
+      })
+    );
+
+    expect(component.eq(1).text()).toEqual(" ");
+    expect(component.eq(2).text()).toEqual("Hello World");
   });
 });
